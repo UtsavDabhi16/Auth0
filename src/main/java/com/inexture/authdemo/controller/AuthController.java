255936b85c9e84df51043aa3d7d1d1e6f283ad55
UtsavDabhi16 <utsav.inexture@gmail.com>
2022-07-13 14:19:51 +0530

    auth-demo

---
+++ src/main/java/com/inexture/authdemo/controller/AuthController.java
@@ -0,0 +1,51 @@
+package com.inexture.authdemo.controller;
+
+import java.io.IOException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import com.inexture.authdemo.config.AuthConfig;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.TestingAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import com.auth0.AuthenticationController;
+import com.auth0.IdentityVerificationException;
+import com.auth0.Tokens;
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.interfaces.DecodedJWT;
+
+@Controller
+public class AuthController {
+
+    @Autowired
+    private AuthConfig config;
+
+    @Autowired
+    private AuthenticationController authenticationController;
+
+    private static final String AUTH0_TOKEN_URL = "https://dev-example.auth0.com/oauth/token";
+
+    @GetMapping(value = "/login")
+    protected void login(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        String redirectUri = config.getContextPath(request) + "/callback";
+        String authorizeUrl = authenticationController.buildAuthorizeUrl(request, response, redirectUri)
+                .withScope("openid email")
+                .build();
+        response.sendRedirect(authorizeUrl);
+    }
+
+    @GetMapping(value="/callback")
+    public void callback(HttpServletRequest request, HttpServletResponse response) throws IOException, IdentityVerificationException {
+        Tokens tokens = authenticationController.handle(request, response);
+
+        DecodedJWT jwt = JWT.decode(tokens.getIdToken());
+        TestingAuthenticationToken authToken2 = new TestingAuthenticationToken(jwt.getSubject(), jwt.getToken());
+        authToken2.setAuthenticated(true);
+
+        SecurityContextHolder.getContext().setAuthentication(authToken2);
+        response.sendRedirect(config.getContextPath(request) + "/");
+    }
+
+
+}
